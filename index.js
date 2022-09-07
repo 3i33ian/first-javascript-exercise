ecd4efb00365fba88927a6f4bab55c63a05a1751
3i33ian <107564430+3i33ian@users.noreply.github.com>
2022-09-07 11:52:07 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const myAge=30
+console.log(myAge);
+
+let earlyYears=2;
+console.log(earlyYears);
+
+earlyYears +=2;
+console.log(earlyYears);
+
+//sottraggo i 2 primi anni ai 30//
+let laterYears= myAge - 2;
+console.log(laterYears);
+
+//moltiplico 28 * 4 //
+laterYears *= 4;
+console.log(laterYears);
+
+myAgeInDogYears = earlyYears + laterYears;
+console.log(myAgeInDogYears);
+
+//eseguo il mio nome a caratteri minuscoli//
+let myName = 'Christian' .toLowerCase();
+console.log(myName);
+
+console.log('My name is ' + myName + ', i am a ' + myAge + ' years old in human years which is ' + myAgeInDogYears + ' years old in dog years.' );
